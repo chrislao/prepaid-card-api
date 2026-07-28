8e53857a9dedd0c6bcc32b3cddd4c3adb1bee798
chrislao <kclaomo@gmail.com>
2026-07-29 01:33:32 +0800

    Remove explicit table mapping

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -8,9 +8,4 @@ public class AppDbContext : DbContext
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
     public DbSet<PrepaidCard> Cards => Set<PrepaidCard>();
-
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
-    {
-        modelBuilder.Entity<PrepaidCard>().ToTable("cards", "public");
-    }
 }
